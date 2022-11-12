1ee502f05dc0583060ca80e448a4f26ea2455e93
MoinMarcell <marcell.dechant@protonmail.com>
2022-11-12 16:37:05 +0100

    add Test SandwichServiceTest

--- src/test/java/com/github/moinmarcell/saftigersandwichshop/service/SandwichServiceTest.java
+++ src/test/java/com/github/moinmarcell/saftigersandwichshop/service/SandwichServiceTest.java
@@ -20,27 +20,42 @@ class SandwichServiceTest {
 
     @Test
     void getAllSandwiches_whenSandwichListIsNotEmpty_thenReturnListOfSandwiches() {
+
         //GIVEN
         List<Sandwich> expected = Collections.emptyList();
+
         //WHEN
         when(sandwichRepo.getAllSandwiches()).thenReturn(expected);
         List<Sandwich> actual = sandwichService.getAllSandwiches();
+
         //THEN
         assertEquals(expected, actual);
         verify(sandwichRepo).getAllSandwiches();
+
     }
 
     @Test
     void getSandwichById_whenIdExist_thenReturnSandwichById() {
+
         //GIVEN
-        Sandwich expected = new Sandwich("1", "sandwich", Collections.emptyList(), 2, false, new Book("1", "title", "author"));
+        Sandwich expected = new Sandwich(
+                "1",
+                "sandwich",
+                Collections.emptyList(),
+                2,
+                false,
+                new Book("1", "title", "author"));
+
         String id = "1";
+
         //WHEN
         when(sandwichRepo.getSandwichById(id)).thenReturn(expected);
         Sandwich actual = sandwichService.getSandwichById(id);
+
         //THEN
         assertEquals(expected, actual);
         verify(sandwichRepo).getSandwichById(id);
+
     }
 
     @Test
@@ -54,19 +69,44 @@ class SandwichServiceTest {
 
     @Test
     void updateSandwich() {
+
         //GIVEN
+        Sandwich expected = new Sandwich(
+                "1",
+                "sandwichneu",
+                Collections.emptyList(),
+                2,
+                false,
+                new Book("1", "title", "author"));
+        String id = "1";
 
         //WHEN
+        when(sandwichRepo.updateSandwich(id, expected)).thenReturn(expected);
+        Sandwich actual = sandwichService.updateSandwich(id, expected);
 
         //THEN
+        assertEquals(expected, actual);
+        verify(sandwichRepo).updateSandwich(id, expected);
     }
 
     @Test
     void deleteSandwich() {
         //GIVEN
+        Sandwich expected = new Sandwich(
+                "1",
+                "sandwichneu",
+                Collections.emptyList(),
+                2,
+                false,
+                new Book("1", "title", "author"));
+        String id = "1";
 
         //WHEN
+        when(sandwichRepo.deleteSandwich(id)).thenReturn(expected);
+        Sandwich actual = sandwichService.deleteSandwich(id);
 
         //THEN
+        assertEquals(expected, actual);
+        verify(sandwichRepo).deleteSandwich(id);
     }
 }
